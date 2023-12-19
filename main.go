48547a3b4c5a4a950a3b865da27ed6b8aeffce5f
Gustav-Smith <gustavojeanpt@gmail.com>
2023-12-19 20:48:52 -0300

    Finished project

--- main.go
+++ main.go
@@ -5,8 +5,18 @@ import (
 	"fmt"
 )
 
+func PagarBoleto(conta verificarConta, valorDoBoleto float64) {
+	conta.Sacar(valorDoBoleto)
+}
+
+type verificarConta interface {
+	Sacar(valor float64) string
+}
+
 func main() {
-	contaExemplo := contas.ContaCorrente{}
-	contaExemplo.Depositar(100)
-	fmt.Println(contaExemplo.ObterSaldo())
+	contaDoGustavo := contas.ContaCorrente{}
+	contaDoGustavo.Depositar(500)
+	PagarBoleto(&contaDoGustavo, 6000)
+
+	fmt.Println(contaDoGustavo.ObterSaldo())
 }
